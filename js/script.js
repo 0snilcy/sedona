5256af751ba5835d152665127a16aff615cea045
miospace <zubov.oleg.7@gmail.com>
2016-03-19 14:23:20 +0300

    add js for form projeck

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// Navigation
+
 var btn = document.querySelector(".main-menu__button");
 var menu = document.querySelector(".main-menu__list");
 
@@ -13,3 +15,47 @@ btn.addEventListener("click", function(event) {
 		btn.classList.remove("main-menu__button--opened");
 	}
 });
+
+
+// Form
+
+var form = document.querySelector(".form");
+var input = form.querySelectorAll("input.form__contacts-input");
+
+form.addEventListener("submit", function(event) {
+	var count = 0;
+	for(var i = 0; i < input.length; i++ ){
+		if(!input[i].value){
+			event.preventDefault();
+			input[i].parentNode.classList.add("form__contacts-wrap--error");
+			count++;
+		}
+	}
+	if (count == "0") {
+		popupGood.classList.remove("popup--closed");
+	}
+});
+
+for(var i = 0; i < input.length; i++){
+	input[i].addEventListener("click", function(event){
+		this.parentNode.classList.remove("form__contacts-wrap--error");
+	});
+}
+
+
+// Popup
+
+var popup     = document.querySelectorAll(".popup");
+var popupBtn  = document.querySelectorAll(".popup__btn");
+var popupBad  = document.querySelector("#popopBad");
+var popupGood = document.querySelector("#popopGood");
+
+for (var i = 0; i < popupBtn.length; i++) {
+	popupBtn[i].addEventListener("click", function(event){
+		for (var i = 0; i < popup.length; i++) {
+			event.preventDefault();
+			popup[i].classList.add("popup--closed");
+		}
+	});
+}
+
