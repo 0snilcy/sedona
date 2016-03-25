45f32c2910bdf63b50d4c32d16cf0e2664386dab
miospace <zubov.oleg.7@gmail.com>
2016-03-25 14:57:41 +0300

    add dynamic map

--- js/script.js
+++ js/script.js
@@ -59,3 +59,24 @@ for (var i = 0; i < popupBtn.length; i++) {
 	});
 }
 
+
+// Map
+
+function initMap() {
+	var myLatLng = {lat: 34.871276, lng: -111.761493};
+	var iconMap = "../img/icon-map-marker.svg";
+
+	var map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 7,
+		center: myLatLng,
+		disableDefaultUI: true
+	});
+
+	var marker = new google.maps.Marker({
+		position: myLatLng,
+		map: map,
+		icon: iconMap,
+		title: 'Hello World!'
+	});
+
+}
